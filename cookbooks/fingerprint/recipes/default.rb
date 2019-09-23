2fafc1316a9bd4ebc1e74dbba80f863c84630492
Nathan Cerny <ncerny@chef.io>
2019-09-23 14:02:54 -0500

    Lots of changes to make things work.
    Signed-off-by: Nathan Cerny <ncerny@chef.io>

--- cookbooks/fingerprint/recipes/default.rb
+++ cookbooks/fingerprint/recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+hab_install 'install habitat' do
+  license 'accept-no-persist'
+end
+
+hab_package 'migration/fingerprinter'
+
+hab_sup 'default' do
+  license 'accept-no-persist'
+end
+
+hab_service 'migration/fingerprinter' do
+  strategy 'at-once'
+  topology 'standalone'
+end
---
+++ habitat/plan.ps1
@@ -0,0 +1,13 @@
+$pkg_name="fingerprinter"
+$pkg_origin="migration"
+$pkg_version="0.1.0"
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_license=("Apache-2.0")
+$pkg_upstream_url="http://chef.io"
+$pkg_build_deps=@("core/chef-dk")
+$pkg_deps=@(
+  "core/cacerts"
+  "stuartpreston/chef-client" # https://github.com/habitat-sh/habitat/issues/6671
+)
+$pkg_scaffolding="chef/scaffolding-chef-infra"
+$scaffold_policy_name="Policyfile"
